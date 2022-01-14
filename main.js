85aff734a7860ae3c2ba2bc538b322f3e7a2a230
Airris Williams <airriswilliams83@gmail.com>
2022-01-14 11:31:00 -0600

    finished code

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-const newKid = (name, grade) => {
-    const createStudent = {
-        name: "Airris",
-        grade: 10,
-    }
+const createStudent = (name, grade) => {
+    const newKid = {
+    name: name,
+    grade: grade
+    }     
+return newKid
 }
+let brandNewStudent = createStudent("Airris",12)
+console.log(brandNewStudent)
+
+const addMathGrade = (studentMath) => {
+    studentMath.math = "B"
+
+    return studentMath
+}
+
+brandNewStudent = addMathGrade(brandNewStudent)
+console.log(brandNewStudent)
+
+const addHistoryGrade = (studentHistory) => {
+    studentHistory.history = "C"
+
+    return studentHistory
+}
+
+brandNewStudent = addHistoryGrade(brandNewStudent)
+console.log(brandNewStudent)
+
+const addScienceGrade = (studentScience) => {
+    studentScience.science = "A"
+
+    return studentScience
+}
+
+brandNewStudent = addScienceGrade(brandNewStudent)
+console.log(brandNewStudent)
